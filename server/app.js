de570b1cdc67d6749680d2d2bca2a1e72f36620e
Yaseen Bhuiyan <ywb9znu@virginia.edu>
2021-06-05 17:46:32 -0400

    created backend
    created firestore database and express api server

---
+++ server/app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+
+const db = require("./firebase");
+
+const port = 8000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/books/get", async (req, res) => {
+  const snapshot = await db.collection("books").get();
+
+  const books = [];
+
+  snapshot.forEach((doc) => {
+    books.push({ ...doc.data(), id: doc.id });
+  });
+  res.send(JSON.stringify(books, undefined, 4));
+});
+
+app.post("/books/add", async (req, res) => {
+  const { title, author } = req.body;
+
+  const resp = await db.collection("books").add({
+    title,
+    author,
+  });
+
+  console.log("Added document with ID: ", resp.id);
+  res.sendStatus(200);
+});
+
+app.delete("/books/delete", async (req, res) => {
+  const { id } = req.body;
+
+  const resp = await db
+    .collection("books")
+    .doc(id)
+    .delete()
+    .then(() => {
+      console.log(`Book ${id} deleted successfully.`);
+    })
+    .catch((error) => {
+      console.error("Error deleting document: ", error);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
+});
